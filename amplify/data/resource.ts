6c694f39705dfbdf26496db2d67c318f94c43eed
Kjetil Geirbo <kjetil@fagfilm.no>
2025-04-15 16:20:24 +0200

    add data

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,53 +1,74 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+import { preSignUp } from "../auth/pre-sign-up/resource";
+// import { licenseValidation } from "../functions/license-validation/resource"
+// import { updateUserLicense } from "./update-user-license/resource"
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any unauthenticated user can "create", "read", "update", 
-and "delete" any "Todo" records.
-=========================================================================*/
 const schema = a.schema({
-  Todo: a
+    // updateUserLicense: a
+    //   .mutation()
+    //   .arguments({
+    //     userId: a.string().required(),
+    //     licenseKey: a.string().required(),
+    //   })
+    //   .authorization((allow) => [allow.group("ADMINS")])
+    //   .handler(a.handler.function(updateUserLicense))
+    //   .returns(a.json()),
+    Todo: a
     .model({
       content: a.string(),
+      done: a.boolean().required().default(false),
+    }),
+    Category: a.model({
+      id: a.id(),
+      name: a.string().required(),
+      slug: a.string().required(),
+      rank: a.integer().required(),
+      videos: a.hasMany('Video', 'categoryId'),
+    }),
+    
+    Video: a.model({
+      id: a.id(),
+      title: a.string().required(),
+      slug: a.string().required(),
+      fileName: a.string().required(),
+      posterTime: a.float(),
+      categoryId: a.id(),
+      category: a.belongsTo('Category', 'categoryId'),
+      chapters: a.hasMany('Chapter', 'videoId'),
+    }),
+  
+    Chapter: a.model({
+      id: a.id(),
+      title: a.string().required().default("untitled"),
+      start: a.float().required(),
+      posterTime: a.float().required().default(10),
+      videoId: a.id(),
+      video: a.belongsTo('Video', 'videoId'),
+    }),
+
+    License: a.model({
+      customer_name: a.string().required(),
+      license_key: a.string().required(),
+      active: a.boolean().required(),
+      start_date: a.date().required(),
     })
-    .authorization((allow) => [allow.guest()]),
-});
+})
+.authorization(allow => [
+  allow.resource(preSignUp).to(['query', 'listen']),
+  allow.authenticated().to(['create', 'update', 'delete', 'read']),
+  // allow.resource(licenseValidation).to(['query', 'listen']),
+  // allow.publicApiKey()
+])
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'iam',
+    // defaultAuthorizationMode: 'iam',
+    defaultAuthorizationMode: 'userPool', // was iam
+    // apiKeyAuthorizationMode: {
+    //   expiresInDays: 30,
+    // },
   },
 });
-
-/*== STEP 2 ===============================================================
-Go to your frontend source code. From your client-side code, generate a
-Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
-WORK IN THE FRONTEND CODE FILE.)
-
-Using JavaScript or Next.js React Server Components, Middleware, Server 
-Actions or Pages Router? Review how to generate Data clients for those use
-cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
-=========================================================================*/
-
-/*
-"use client"
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-
-const client = generateClient<Schema>() // use this Data client for CRUDL requests
-*/
-
-/*== STEP 3 ===============================================================
-Fetch records from the database and use them in your frontend component.
-(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
-=========================================================================*/
-
-/* For example, in a React component, you can use this snippet in your
-  function's RETURN statement */
-// const { data: todos } = await client.models.Todo.list()
-
-// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
